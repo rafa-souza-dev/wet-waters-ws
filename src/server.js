9dbb37face415a74a788a021421d72d53dbb649f
Rafael Souza <rafaeljosegomes25@gmail.com>
2023-09-17 15:25:24 -0300

    show clients size

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const wss = new WebSocket.WebSocketServer({ host: '0.0.0.0', port: 8080 });
 
 wss.on('connection', function connection(ws) {
   console.log("conectou!")
+  console.log("clients: ", wss.clients.size)
 
   ws.on('error', console.error);
 
